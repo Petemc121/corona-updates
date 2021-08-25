7fd3bb7b7e98de721175b63733aa457dcddce5b5
Petemc121 <petemc121@gmail.com>
2021-08-25 20:37:41 +0900

    Succesfully integrated data into the graphs. Now all that's left is formatting.

--- src/Graphs.js
+++ src/Graphs.js
@@ -1,54 +1,88 @@
-import React, {useState} from "react";
-import JSCharting from "jscharting-react"
-import GraphCountries from "./GraphCountries"
-
-
-
-
+import React, { useState } from "react";
+import JSCharting from "jscharting-react";
+import GraphCountries from "./GraphCountries";
 
 export default function Graphs({ displayHandler, covidData, covidHistory }) {
-  console.log(covidHistory)
+  console.log(covidHistory);
   const initConfig = {
-    type: 'line',
+    type: "line",
     series: [
-        {
-            points: [
-                
-            ]
-        }
-    ]
-  };
-  const [config, setConfig] = useState({country:"undefined", configuration:initConfig})
-
-  const handleGraphCountryChange= (e) => {
-      if (e.target.value === "Select a country")
       {
-        return
+        points: [],
+      },
+    ],
+  };
+  const [config, setConfig] = useState({
+    country: "undefined",
+    configuration: initConfig,
+  });
+
+  const handleGraphCountryChange = (e) => {
+    if (e.target.value === "Select a country") {
+      return;
+    }
+
+    console.log(e.target.value);
+
+    const countryData = covidHistory.filter(
+      (datapoint) => datapoint.Country === e.target.value
+    );
+
+    const formatedCountryData = countryData.map((datapoint) => {
+      return { x: datapoint.date, y: datapoint.total_cases };
+    });
+
+    const filteredCountryData = formatedCountryData.filter(datapoint => datapoint.y > 0)
+
+    const sortedCountryData = filteredCountryData.sort((a, b) => {
+      if (a.y < b.y) {
+        return -1;
       }
 
-      console.log(e.target.value)
+      if (a.y > b.y) {
+        return 1;
+      }
 
-    const countryData = covidHistory.map((datapoint,index) => {
-      return datapoint.Country = e.target.value
+      return 0;
+    });
 
-    })
 
     console.log(countryData);
+    console.log(formatedCountryData);
+    setConfig({
+      country: e.target.value,
+      configuration: {
+        series: [
+          {
+            points: sortedCountryData,
+            scales: {
+              xAxes: [
+                {
+                  type: "time",
+                  time: {
+                    unit: "month",
+                    unitStepSize: 1,
+                  },
+                },
+              ],
+            },
+          },
+        ],
+      },
+    });
+  };
 
-    // setConfig({country:e.target.value, configuration:{series: [{points: countryData}]}})
-      
-
-  }
-
-
-
-
-  return <div style={{ display: displayHandler() }} id="graphsContain">
-<div style={{height:"300px"}}>
-<JSCharting options={config.configuration} />
-</div>
-<div class="center">
-<GraphCountries graphCountryChange={handleGraphCountryChange} covidData={covidData} /> 
-</div>
-  </div>;
+  return (
+    <div style={{ display: displayHandler() }} id="graphsContain">
+      <div style={{ height: "300px" }}>
+        <JSCharting options={config.configuration} />
+      </div>
+      <div class="center">
+        <GraphCountries
+          graphCountryChange={handleGraphCountryChange}
+          covidData={covidData}
+        />
+      </div>
+    </div>
+  );
 }
