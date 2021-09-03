14d48f77cb926d6acb49fc31d56e33a6fb5bd11c
Petemc121 <petemc121@gmail.com>
2021-09-03 15:25:56 +0900

    sorted countries

--- src/Countries.js
+++ src/Countries.js
@@ -2,11 +2,27 @@ import React from "react";
 import Country from "./Country";
 
 export default function Countries({ countryChange, covidData }) {
-  return (
+    const sortedCovidData = covidData.sort(function(a, b) {
+      var countryA = a.Country.toUpperCase(); // ignore upper and lowercase
+      var countryB = b.Country.toUpperCase(); // ignore upper and lowercase
+      if (countryA < countryB) {
+        return -1;
+      }
+      if (countryA > countryB) {
+        return 1;
+      }
+    
+      // names must be equal
+      return 0;
+    });
+
+    return (
+     
     <select onChange={countryChange} id="countrySelect">
+       {console.log(sortedCovidData)}
       <option default="selected">Select a country</option>
-      {covidData.map((datapoint, index) => {
-        if (index > 0 && datapoint.County !== "Total:") {
+      {sortedCovidData.map((datapoint, index) => {
+        if (index > 0 && datapoint.Country !== "Total:") {
           return <Country key={index} country={datapoint.Country} />;
         }
       })}
